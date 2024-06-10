3e2840823f733ab0e12e2a6061e60e7a6ef7e7ee
Rafael Cardinali <rflcardinali@gmail.com>
2024-06-10 11:04:06 -0300

    feat(ui): Enhance user interface with filter functionality and styling
    - Implement filter functionality to dynamically search users by name.
    
    - Update map and user list components in response to filter changes.
    
    - Improve visual appeal of the filter input with Tailwind CSS styling.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import { User } from '../../models/types';
 import 'leaflet/dist/leaflet.css';
 
-var myIcon = L.icon({
+let myIcon = L.icon({
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
@@ -18,9 +18,15 @@ interface MapProps {
 }
 
 const Markers: React.FC<MapProps> = ({ users }) => {
-  const map = useMapEvents({});
+  const map = useMap();
 
   useEffect(() => {
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        map.removeLayer(layer);
+      }
+    });
+
     users.forEach(user => {
       L.marker([parseFloat(user.address.geo.lat), parseFloat(user.address.geo.lng)], { icon: myIcon })
         .addTo(map)
@@ -42,10 +48,9 @@ const Markers: React.FC<MapProps> = ({ users }) => {
 
 const Map: React.FC<MapProps> = ({ users }) => {
   return (
-    <MapContainer center={{ lat: 0, lng: 0 }} zoom={2} style={{ height: "100vh", width: "75%" }}>
+    <MapContainer center={[0, 0]} zoom={2} style={{ height: '100vh', width: '75%' }}>
       <TileLayer
-        url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=qwDbex8h0YUY0kIVRYYk"
-        attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Markers users={users} />
     </MapContainer>
